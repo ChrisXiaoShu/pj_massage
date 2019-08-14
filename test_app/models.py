5ce3e6cce6457859d1882768f4165f4bacff8db4
ChrisXiaoShu <52444096+ChrisXiaoShu@users.noreply.github.com>
2019-08-14 17:53:12 +0800

    new model customer

--- test_app/models.py
+++ test_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class customer(models.Model):
+    cName = models.CharField(max_length=10, null=False)
+    cPhone = models.CharField(max_length=10, null=False)
+    def __str__(self):
+        return "{}-{}".format(self.cName, self.cPhone)
