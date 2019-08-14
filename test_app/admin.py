c2c78b139a18c3b4b5a760e176087cb34e795aaa
ChrisXiaoShu <52444096+ChrisXiaoShu@users.noreply.github.com>
2019-08-14 18:31:41 +0800

    add field->isBlack

--- test_app/admin.py
+++ test_app/admin.py
@@ -3,7 +3,8 @@ from test_app.models import customer
 
 # Register your models here.
 class customerAdmin(admin.ModelAdmin):
-    list_display=('id', 'cName', 'cPhone')
+    list_display=('id', 'cName', 'cPhone','isBlack')
+    list_filter=('isBlack',)
     search_fields=('cName','cPhone')
     ordering=('id',)
 
--- test_app/models.py
+++ test_app/models.py
@@ -3,5 +3,6 @@ from django.db import models
 class customer(models.Model):
     cName = models.CharField(max_length=10, null=False)
     cPhone = models.CharField(max_length=10, null=False)
+    isBlack = models.BooleanField(default=False)
     def __str__(self):
         return "{}-{}".format(self.cName, self.cPhone)
