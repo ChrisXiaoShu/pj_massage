fff1a0f41c5ab0c7c3e61000df1a60600251bf09
ChrisXiaoShu <52444096+ChrisXiaoShu@users.noreply.github.com>
2019-08-14 18:15:32 +0800

    add customer admin class

--- test_app/admin.py
+++ test_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from test_app.models import customer
 
 # Register your models here.
+class customerAdmin(admin.ModelAdmin):
+    list_display=('id', 'cName', 'cPhone')
+    search_fields=('cName','cPhone')
+    ordering=('id',)
+
+
+admin.site.register(customer, customerAdmin)
