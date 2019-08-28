f83d7559691f8941857b9c4aa2efd1c7fff832e5
ChrisXiaoShu <52444096+ChrisXiaoShu@users.noreply.github.com>
2019-08-28 23:24:04 +0800

    create all backend model

--- test_app/admin.py
+++ test_app/admin.py
@@ -1,12 +1,33 @@
 from django.contrib import admin
-from test_app.models import customer
+from test_app.models import Customer, Reservation, MassagerGroup, Massager
 
 # Register your models here.
-class customerAdmin(admin.ModelAdmin):
-    list_display=('id', 'cName', 'cPhone','isBlack')
-    list_filter=('isBlack',)
-    search_fields=('cName','cPhone')
+class CustomerAdmin(admin.ModelAdmin):
+    list_display=('id', 'line_id', 'c_name', 'c_phone','is_black')
+    list_filter=('is_black',)
+    search_fields=('c_name','c_phone')
     ordering=('id',)
 
 
-admin.site.register(customer, customerAdmin)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display=('id', 'customer', 'date', 'c_name', 'c_phone')
+    list_filter=('date',)
+    search_fields=('customer','c_name')
+    ordering=('date',)
+
+
+class MassagerGroupAdmin(admin.ModelAdmin):
+    list_display=('id', 'g_name')
+    ordering=('g_name',)
+
+class MassagerAdmin(admin.ModelAdmin):
+    list_display=('id', 'm_name', 'massagergroup')
+    list_filter=('massagergroup',)
+    search_fields=('m_name','massagergroup')
+    ordering=('massagergroup',)
+
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Reservation, ReservationAdmin)
+admin.site.register(MassagerGroup, MassagerGroupAdmin)
+admin.site.register(Massager, MassagerAdmin)
--- test_app/models.py
+++ test_app/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class Customer(models.Model):
-    line_id = models.CharField()
+    line_id = models.CharField(max_length=30, null=False)
     c_name = models.CharField(max_length=10, null=False)
     c_phone = models.CharField(max_length=10, null=False)
     is_black = models.BooleanField(default=False)
 
     def __str__(self):
-        return "{}_{}".format(self.cName, self.cPhone)
+        return "{}_{}".format(self.c_name, self.c_phone)
 
 
 class Reservation(models.Model):
@@ -15,3 +15,14 @@ class Reservation(models.Model):
     date = models.DateTimeField()
     c_name = models.CharField(max_length=10, null=False)
     c_phone = models.CharField(max_length=10, null=False)
+
+
+class MassagerGroup(models.Model):
+    g_name = models.CharField(max_length=10, null=False)
+
+    def __str__(self):
+        return "{}{}".format('massage group ', self.g_name)
+
+class Massager(models.Model):
+    massagergroup = models.ForeignKey(MassagerGroup, on_delete=models.CASCADE)
+    m_name = models.CharField(max_length=10, null=False)
