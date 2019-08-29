0d8d0e62ddd6e56446d9050fb10889ffdfb0270f
ChrisXiaoShu <52444096+ChrisXiaoShu@users.noreply.github.com>
2019-08-29 15:21:27 +0800

    make_reservation_function

--- test_app/models.py
+++ test_app/models.py
@@ -16,6 +16,26 @@ class Reservation(models.Model):
     c_name = models.CharField(max_length=10, null=False)
     c_phone = models.CharField(max_length=10, null=False)
 
+@classmethod
+def make_reservation(reservation, massager, date, customer, c_name, c_phone):
+    status = False
+    # with transaction.atomic():
+    #     q_reservation = (
+    #        reservation.objects
+    #        .select_for_update()
+    #        .get(customer = customer, massager = massager, date = date)
+    #     )
+
+    #     if not q_reservation:
+    #         reservation.objects.create()
+    #         status = True
+    obj, created = Person.objects.get_or_create(
+    first_name='John',
+    last_name='Lennon',
+    defaults={'birthday': date(1940, 10, 9)},
+)   
+          
+    return status
 
 class MassagerGroup(models.Model):
     g_name = models.CharField(max_length=10, null=False)
